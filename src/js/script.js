93b21c195be767c5d191c9b0dece01245ac4428a
Rafael <falaguastaferrazrafael@gmail.com>
2025-03-28 11:42:01 -0300

    declaração de variaveis

---
+++ src/js/script.js
@@ -0,0 +1,19 @@
+// DECLARAÇÃO E VARIAVEIS
+
+var nome ="Fiap";
+console.log(nome)
+
+let idade =19
+console.log(idade)
+
+const sobrenome ="Fiapinho"
+console.log(sobrenome)
+
+//undefined
+
+let exemplo;
+console.log(exemplo)
+
+//tipos de variaveis
+
+let 
